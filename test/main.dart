4bedb0104c5fe7081e4134a0af4582406c00ce63
kzhdev <kun.zhao@tradingtechnologies.com>
2014-04-22 13:15:38 -0500

    add more test

--- test/main.dart
+++ test/main.dart
@@ -1,12 +1,32 @@
 import 'package:unittest/unittest.dart';
 
-import '../lib/collection_ext.dart';
+import '../lib/dart_ext.dart';
 
 void main() {
+  test('capitalize', () {
+    String s = capitalize('internet');
+    expect(s, equals('Internet'));
+  });
+
+  test('clone', (){
+    Map a = {'one': 1, 'two': 2, 'map': {'one': 1, 'two': 2}};
+    Map b = clone(a);
+    expect(b, equals(a));
+    b['two'] = 'two';
+    b['map']['two'] = 'Two';
+    expect(b, equals({'one': 1, 'two': 'two', 'map': {'one': 1, 'two': 'Two'}}));
+    expect(a, equals({'one': 1, 'two': 2, 'map': {'one': 1, 'two': 2}}));
+  });
+
   test('merge', () {
     Map one = { 'a': 'a', 'b': 'b', 'list': [{'one': 1}, {'two': 2}]};
     Map two = { 'a': 'A', 'b': 3, 'list': [{'two': 2}, {'three': 3}, 4]};
     Map three = merge(one, two);
     expect(three, equals({'a': 'A', 'b': 3, 'list': [{'one': 1, 'two': 2}, {'two': 2, 'three': 3}, 4]}));
+
+    // changing merged map shouldn't change original maps
+    three['b'] = 'B';
+    expect(two['b'], equals(3));
+    expect(one['b'], equals('b'));
   });
 }
